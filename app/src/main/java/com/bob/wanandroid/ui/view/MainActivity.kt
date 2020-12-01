76d50236e399a7e8ba79bca67ef865346c30310a
chenliangyu <3063093332@qq.com>
2020-12-01 15:24:07 +0800

    cc test

--- app/src/main/java/com/bob/wanandroid/ui/view/MainActivity.kt
+++ app/src/main/java/com/bob/wanandroid/ui/view/MainActivity.kt
@@ -3,20 +3,34 @@ package com.bob.wanandroid.ui.view
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
+import com.billy.cc.core.component.CC
 import com.bob.base.ui.base.BaseActivity
+import com.bob.common.router.FindRouter
 import com.bob.wanandroid.R
 import com.bob.wanandroid.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
-    private lateinit var fragments: Map<Int, Fragment>
+    private lateinit var fragments: Map<Int, Fragment?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         fragments = mapOf(
-
+            R.id.home to createHomeFragment()
         )
+    }
+
+    private fun createHomeFragment(): Fragment? {
+        var fragment: Fragment? = null
+        CC.obtainBuilder(FindRouter.componentName)
+            .setActionName(FindRouter.createFindFragment)
+            .build().callAsyncCallbackOnMainThread { _, result ->
+                if (result.isSuccess) {
+                    fragment = result.getDataItemWithNoKey()
+                }
+            }
+        return fragment
     }
 }
